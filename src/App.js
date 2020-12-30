5b1748e1094c0a08b71548a97a8868467d394932
kpandian <kpandian@ucsc.edu>
2020-12-29 21:32:19 -0800

    Pushing changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { HashRouter as Router, Switch, Route} from "react-router-dom";
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import Body from './Components/Body/Body';
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './Header.css';
 
-
-
 const Header = () => {
     return(
         <>
